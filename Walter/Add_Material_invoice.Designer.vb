fe61e19eee3563a64336f7687e981c7f3f8942d2
Ted Eriksson <ted.eriksson@me.com>
2011-03-04 11:50:15 +0000

    ADI no buttons

--- Walter/Add_Material_invoice.Designer.vb
+++ Walter/Add_Material_invoice.Designer.vb
@@ -23,7 +23,6 @@ Partial Class Add_Material_invoice
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.ADM_Supplier_Name = New System.Windows.Forms.ComboBox()
-        Me.ADM_Hours_worked = New System.Windows.Forms.TextBox()
         Me.ADM_Cost = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
@@ -32,26 +31,22 @@ Partial Class Add_Material_invoice
         Me.ADM_Cancel = New System.Windows.Forms.Button()
         Me.AMI_Done = New System.Windows.Forms.Button()
         Me.AMI_Save_and_new = New System.Windows.Forms.Button()
+        Me.AMI_Job_Name = New System.Windows.Forms.ComboBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'ADM_Supplier_Name
         '
         Me.ADM_Supplier_Name.FormattingEnabled = True
-        Me.ADM_Supplier_Name.Location = New System.Drawing.Point(116, 37)
+        Me.ADM_Supplier_Name.Location = New System.Drawing.Point(116, 59)
         Me.ADM_Supplier_Name.Name = "ADM_Supplier_Name"
         Me.ADM_Supplier_Name.Size = New System.Drawing.Size(156, 21)
         Me.ADM_Supplier_Name.TabIndex = 35
         '
-        'ADM_Hours_worked
-        '
-        Me.ADM_Hours_worked.Location = New System.Drawing.Point(116, 90)
-        Me.ADM_Hours_worked.Name = "ADM_Hours_worked"
-        Me.ADM_Hours_worked.Size = New System.Drawing.Size(156, 20)
-        Me.ADM_Hours_worked.TabIndex = 34
-        '
         'ADM_Cost
         '
-        Me.ADM_Cost.Location = New System.Drawing.Point(116, 64)
+        Me.ADM_Cost.Location = New System.Drawing.Point(116, 86)
         Me.ADM_Cost.Name = "ADM_Cost"
         Me.ADM_Cost.Size = New System.Drawing.Size(156, 20)
         Me.ADM_Cost.TabIndex = 33
@@ -59,16 +54,16 @@ Partial Class Add_Material_invoice
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(12, 93)
+        Me.Label4.Location = New System.Drawing.Point(12, 36)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(79, 13)
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
         Me.Label4.TabIndex = 32
-        Me.Label4.Text = "Hours Worked:"
+        Me.Label4.Text = "Job Name:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(12, 67)
+        Me.Label3.Location = New System.Drawing.Point(12, 89)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(31, 13)
         Me.Label3.TabIndex = 31
@@ -77,7 +72,7 @@ Partial Class Add_Material_invoice
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Location = New System.Drawing.Point(12, 63)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(79, 13)
         Me.Label2.TabIndex = 30
@@ -94,7 +89,7 @@ Partial Class Add_Material_invoice
         '
         'ADM_Cancel
         '
-        Me.ADM_Cancel.Location = New System.Drawing.Point(197, 116)
+        Me.ADM_Cancel.Location = New System.Drawing.Point(197, 138)
         Me.ADM_Cancel.Name = "ADM_Cancel"
         Me.ADM_Cancel.Size = New System.Drawing.Size(75, 23)
         Me.ADM_Cancel.TabIndex = 36
@@ -103,7 +98,7 @@ Partial Class Add_Material_invoice
         '
         'AMI_Done
         '
-        Me.AMI_Done.Location = New System.Drawing.Point(116, 116)
+        Me.AMI_Done.Location = New System.Drawing.Point(116, 138)
         Me.AMI_Done.Name = "AMI_Done"
         Me.AMI_Done.Size = New System.Drawing.Size(75, 23)
         Me.AMI_Done.TabIndex = 37
@@ -112,23 +107,50 @@ Partial Class Add_Material_invoice
         '
         'AMI_Save_and_new
         '
-        Me.AMI_Save_and_new.Location = New System.Drawing.Point(15, 116)
+        Me.AMI_Save_and_new.Location = New System.Drawing.Point(15, 138)
         Me.AMI_Save_and_new.Name = "AMI_Save_and_new"
         Me.AMI_Save_and_new.Size = New System.Drawing.Size(95, 23)
         Me.AMI_Save_and_new.TabIndex = 38
         Me.AMI_Save_and_new.Text = "Save and New"
         Me.AMI_Save_and_new.UseVisualStyleBackColor = True
         '
+        'AMI_Job_Name
+        '
+        Me.AMI_Job_Name.FormattingEnabled = True
+        Me.AMI_Job_Name.Location = New System.Drawing.Point(116, 33)
+        Me.AMI_Job_Name.Name = "AMI_Job_Name"
+        Me.AMI_Job_Name.Size = New System.Drawing.Size(156, 21)
+        Me.AMI_Job_Name.TabIndex = 39
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker1.Location = New System.Drawing.Point(116, 112)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(156, 20)
+        Me.DateTimePicker1.TabIndex = 40
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 118)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 13)
+        Me.Label5.TabIndex = 41
+        Me.Label5.Text = "Date of Invoice:"
+        '
         'Add_Material_invoice
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 151)
+        Me.ClientSize = New System.Drawing.Size(284, 172)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.AMI_Job_Name)
         Me.Controls.Add(Me.AMI_Save_and_new)
         Me.Controls.Add(Me.AMI_Done)
         Me.Controls.Add(Me.ADM_Cancel)
         Me.Controls.Add(Me.ADM_Supplier_Name)
-        Me.Controls.Add(Me.ADM_Hours_worked)
         Me.Controls.Add(Me.ADM_Cost)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
@@ -141,7 +163,6 @@ Partial Class Add_Material_invoice
 
     End Sub
     Friend WithEvents ADM_Supplier_Name As System.Windows.Forms.ComboBox
-    Friend WithEvents ADM_Hours_worked As System.Windows.Forms.TextBox
     Friend WithEvents ADM_Cost As System.Windows.Forms.TextBox
     Friend WithEvents Label4 As System.Windows.Forms.Label
     Friend WithEvents Label3 As System.Windows.Forms.Label
@@ -150,4 +171,7 @@ Partial Class Add_Material_invoice
     Friend WithEvents ADM_Cancel As System.Windows.Forms.Button
     Friend WithEvents AMI_Done As System.Windows.Forms.Button
     Friend WithEvents AMI_Save_and_new As System.Windows.Forms.Button
+    Friend WithEvents AMI_Job_Name As System.Windows.Forms.ComboBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label5 As System.Windows.Forms.Label
 End Class
--- Walter/Add_Material_invoice.vb
+++ Walter/Add_Material_invoice.vb
@@ -39,4 +39,23 @@ Public Class Add_Material_invoice
             Me.Enabled = False
         End If
     End Sub
+
+
+    Private Sub AMI_Job_Name_GotFocus(ByVal sender As Object, ByVal e As System.EventArgs) Handles AMI_Job_Name.GotFocus
+        AMI_Job_Name.Items.Clear()
+        Dim myConnection As New OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;Data Source=" & Application.StartupPath & "\..\..\walter_database.mdb")
+        Dim mySelectQuery As String = "SELECT Job_Name FROM Jobs"
+        Dim myCommand As New OleDbCommand(mySelectQuery, myConnection)
+        myConnection.Open()
+        Dim myReader As OleDbDataReader
+        myReader = myCommand.ExecuteReader()
+        ' Always call Read before accessing data.
+        While myReader.Read()
+            AMI_Job_Name.Items.Add(myReader.GetValue(0)) 'AAAWWW YEEEAAAHHHH'
+        End While
+        ' always call Close when done reading.
+        myReader.Close()
+        ' Close the connection when done with it.
+        myConnection.Close()
+    End Sub
 End Class
