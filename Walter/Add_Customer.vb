efeb4784daf6838e8a02ca0febea0bd827e34231
Ted Eriksson <ted.eriksson@me.com>
2011-03-06 17:51:34 +0000

    All sql statements now work through SQLStuff.vb

--- Walter/Add_Customer.vb
+++ Walter/Add_Customer.vb
@@ -3,22 +3,7 @@
 Public Class Add_Customer
 
     Private Sub NJ_Done_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles NJ_Done.Click
-
-        'SQL Stuff that took bloody ages'
-        Dim conn As New OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;Data Source=" & Application.StartupPath & "\..\..\walter_database.mdb")
-        'Dim adapter As New OleDbDataAdapter("select * FROM Contacts", conn)'
-        Dim sql As String = String.Empty
-        sql = "INSERT INTO Customer (Customer_Name, Address1, Address2, Postcode) VALUES('" + AC_Customer_name.Text + "', '" + AC_Address1.Text + "', '" + AC_Address2.Text + "', '" + AC_Postcode.Text + "')"
-        'MsgBox(sql)
-        conn.Open()
-        Dim command As New OleDbCommand(sql, conn)
-        Dim rowsAffected As Integer
-        rowsAffected = command.ExecuteNonQuery()
-        MsgBox("Query affected " & rowsAffected & " rows!")
-        conn.Close()
-
-
-
+        SQLWriting("INSERT INTO Customer (Customer_Name, Address1, Address2, Postcode) VALUES('" + AC_Customer_name.Text + "', '" + AC_Address1.Text + "', '" + AC_Address2.Text + "', '" + AC_Postcode.Text + "')")
         NewJob.NJ_Customer.Text = AC_Customer_name.Text
         NewJob.Enabled = True
         Me.Close()
--- Walter/Add_Supplier.vb
+++ Walter/Add_Supplier.vb
@@ -2,20 +2,7 @@
 Public Class Add_Supplier
 
     Private Sub AS_Done_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles AS_Done.Click
-
-        'SQL Stuff that took bloody ages'
-        Dim conn As New OleDbConnection("Provider=Microsoft.Jet.OleDb.4.0;Data Source=" & Application.StartupPath & "\..\..\walter_database.mdb")
-        'Dim adapter As New OleDbDataAdapter("select * FROM Contacts", conn)'
-        Dim sql As String = String.Empty
-        sql = "INSERT INTO Supplier (Supplier_Name, Address1, Address2, Postcode) VALUES('" + AS_Supplier_name.Text + "', '" + AS_Address1.Text + "', '" + AS_Address2.Text + "', '" + AS_Postcode.Text + "')"
-        'MsgBox(sql)
-        conn.Open()
-        Dim command As New OleDbCommand(sql, conn)
-        Dim rowsAffected As Integer
-        rowsAffected = command.ExecuteNonQuery()
-        MsgBox("Query affected " & rowsAffected & " rows!")
-        conn.Close()
-
+        SQLWriting("INSERT INTO Supplier (Supplier_Name, Address1, Address2, Postcode) VALUES('" + AS_Supplier_name.Text + "', '" + AS_Address1.Text + "', '" + AS_Address2.Text + "', '" + AS_Postcode.Text + "')")
         Add_Material_invoice.ADM_Supplier_Name.Text = AS_Supplier_name.Text
         Add_Material_invoice.Enabled = True
         Me.Close()
