1645a00bda070b8ef6ef264336ca885542cdf71b
Ted <ted.eriksson@me.com>
2011-03-14 10:58:04 +0000

    Revert "Started to add the ability to check a jobs status"
    This reverts commit a7efb7b261819fb02fc2486f799730afae5eeac6.

--- Walter/Results.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Results
-
-    Private Sub R_Job_status_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles R_Job_status.Click
-        Job_Status.Visible = True
-        Me.Close()
-    End Sub
-End Class
